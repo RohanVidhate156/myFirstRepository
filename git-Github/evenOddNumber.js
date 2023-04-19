d9a476a672e2c53a240baffdba57aeb18a4796b5
RohanVidhate156 <rohanvidhate582000@gmail.com>
2023-04-19 19:31:11 +0530

    IsEvenOrOddNumber code

--- git-Github/evenOddNumber.js
+++ git-Github/evenOddNumber.js
@@ -1,1 +1,9 @@
-prompt("Enter the number ")
+
+// To find even or odd numbers betwen 1 to 100.
+for(let i=1;i<=100;i++){
+    if (i%2==0)
+        console.log(i,"is a even number.");
+    else{
+        console.log(i,"is an odd number.");
+    }
+}
