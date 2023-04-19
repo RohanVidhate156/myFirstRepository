c09d837d5470f5f952e041c413a366a26175c7eb
RohanVidhate156 <rohanvidhate582000@gmail.com>
2023-04-19 21:27:19 +0530

    IsNumberIsPrimeOrnot javascript code

---
+++ git-Github/isPrimenoOrNot.js
@@ -0,0 +1,12 @@
+// Find Number is prime or not:
+let temp=0;
+for(let i=2;i<11;i++){
+    if(11%i==0)
+    temp++
+}
+if(temp>1){
+    console.log("Number is not prime")
+}
+else{
+    console.log("Number is prime")
+}
